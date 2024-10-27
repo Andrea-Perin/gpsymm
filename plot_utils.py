34f5f3f11d6413d89f0169cbf388aa9705ec0752
Andrea Perin <xinandre@gmail.com>
2024-10-27 19:19:02 +0200

    tweaks to plotting standards

--- plot_utils.py
+++ plot_utils.py
@@ -14,14 +14,14 @@ def cloudplot(xvals, yvals, cvals, xlabel, ylabel, clabel, titles, figsize):
     assert len(xvals) == len(yvals) == len(cvals) == len(titles)
     n_rots = len(xvals)
     fig = plt.figure(figsize=figsize)
-    fig.supxlabel(xlabel, y=-0.15)
-    fig.supylabel(ylabel, x=0.07)
+    fig.supxlabel(xlabel)
+    fig.supylabel(ylabel)
     grid = ImageGrid(
         fig, 111, nrows_ncols=(1, n_rots), axes_pad=0.1, label_mode="L", share_all=True,
         cbar_location="right", cbar_mode="single", cbar_size="5%", cbar_pad=0.1, aspect=False)
     for ax, xs, ys, cs, title in zip(grid, xvals, yvals, cvals, titles):
         ax.set_title(title)
-        im = ax.scatter(xs, ys, marker='.', alpha=.1, s=2, c=cs, cmap='viridis')
+        im = ax.scatter(xs, ys, marker='.', alpha=.1, s=2, c=cs)
     # colorbar
     cbar = ax.cax.colorbar(plt.cm.ScalarMappable(cmap=im.get_cmap(), norm=im.norm))
     cbar.set_label(clabel)
