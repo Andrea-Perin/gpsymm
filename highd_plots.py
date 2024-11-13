5c253ecd3c0af048b5e746c89e3e81a6cf5fb1d3
Andrea Perin <xinandre@gmail.com>
2024-11-13 16:32:07 +0200

    tweaks to high d plots

--- highd_plots.py
+++ highd_plots.py
@@ -1,5 +1,6 @@
 # %% This thing produces just the plots from the given data
 import numpy as np
+import einops as ein
 from highd import N_ROTATIONS
 from mnist_utils import load_images, load_labels, normalize_mnist
 from data_utils import three_shear_rotate, xshift_img, kronmap
@@ -30,25 +31,29 @@ out_path = Path('images') / 'highd_trained'
 data_path = Path('results') / 'highd_trained_adafactor'
 
 
-# %% Load data
-losses = np.load( data_path / 'losses.npy' )
-theory_values = np.load( data_path / 'theory_values.npy' )
-preds = np.load( data_path / 'predictions.npy' )
 
-# %%
-x = np.arange(10)
-y = np.arange(10)
-plt.scatter(x, y, c=x**2+y**2, cmap=mycmap)
-plt.colorbar()
-plt.show()
-# %%
+# %% Load data
+losses = np.load( data_path / 'losses.npy' )[ROT_IDX]
+
+preds = np.load( data_path / 'predictions.npy' )[ROT_IDX]
+correct_p = preds[..., 0] > 0
+correct_m = preds[..., 1] < 0
+corr_count = np.sum( np.column_stack([correct_p, correct_m]), axis=1 )
+emp_err = np.mean( np.abs(preds - np.array([+1., -1.])), axis=-1 )
+
+theory_values = ein.rearrange(
+    np.load( data_path / 'theory_values.npy' )[ROT_IDX],
+    'pair value -> value pair'
+)
+sp_err, lambda_n, lambda_avg, deltasq, avg_angle = theory_values
+
 
 
 
 
 
 # %% Spectral vs. empirical error
-semaphore = plt.color.ListedColormap(['red', 'yellow', 'green'])
+semaphore = mpl.colors.ListedColormap(['red', 'yellow', 'green'])
 fig = plt.figure(figsize=(5.75*cm, 5*cm))
 grid = ImageGrid(
     fig, 111, nrows_ncols=(1, 1), axes_pad=0.1, label_mode="L", share_all=True,
@@ -56,13 +61,7 @@ grid = ImageGrid(
 grid[0].set_title(f"$N_{{rot}}={{{N_ROTATIONS[ROT_IDX]}}}$", fontsize=10)
 grid[0].set_xlabel("Empirical error")
 grid[0].set_ylabel("Spectral error")
-im = grid[0].scatter(
-    empirical_errors[rot_idx],
-    spectral_errors[rot_idx],
-    c=emp_counts[rot_idx],
-    marker='.', alpha=.1, s=2,
-    cmap=semaphore, norm=norm
-)
+im = grid[0].scatter( emp_err, sp_err, c=corr_count, marker='.', alpha=.1, s=2, cmap=semaphore )
 cbar = grid.cbar_axes[0].colorbar(
     plt.cm.ScalarMappable(norm=im.norm, cmap=im.get_cmap()),
     label='Num. correct',
@@ -72,33 +71,11 @@ cbar = grid.cbar_axes[0].colorbar(
 cbar.ax.tick_params(size=0)
 cbar.ax.set_yticklabels((0, 1, 2))
 
-cmax = max(empirical_errors[rot_idx].max(), spectral_errors[rot_idx].max())
+cmax = max(emp_err.max(), sp_err.max())
 grid[0].plot([0, cmax], [0, cmax], color='black', alpha=1, lw=.75, ls='--')
 grid[0].set_xticks([0., 0.5, 1., 1.5])
 grid[0].set_xlim((0, None))
 grid[0].set_ylim((0, None))
 plt.tight_layout(pad=0.4)
-plt.savefig(out_path / f'panelB_semaphore_{N_ROTATIONS[rot_idx]}.pdf')
-plt.show()
-# %% PANEL C: v2
-fig = plt.figure(figsize=(5.75*cm, 5*cm))
-grid = ImageGrid(
-    fig, 111, nrows_ncols=(1, 1), axes_pad=0.1, label_mode="L", share_all=True,
-    cbar_location="right", cbar_mode="single", cbar_size="5%", cbar_pad=0.1, aspect=False)
-# grid[0].ticklabel_format(axis='x', style='sci', scilimits=(0,0))
-grid[0].set_title(f"$N_{{rot}}={{{N_ROTATIONS[rot_idx]}}}$", fontsize=10)
-grid[0].set_ylabel(r"$\Delta^2$")
-grid[0].set_xlabel(r"$\lambda_N$")
-im = grid[0].scatter(
-    (1/lambda_last[rot_idx]),
-    deltasq[rot_idx],
-    c=jnp.log(spectral_errors[rot_idx]),
-    marker='.', alpha=.1, s=2)
-cbar = grid.cbar_axes[0].colorbar(
-    plt.cm.ScalarMappable(norm=im.norm, cmap=im.get_cmap()), alpha=1.0
-)
-cbar.ax.set_title(r'$\log\varepsilon_s$', fontsize=10)
-grid[0].yaxis.set_tick_params(rotation=90)
-plt.tight_layout(pad=0.4)
-# plt.savefig(out_path / f'panelC_{N_ROTATIONS[rot_idx]}.pdf')
+# plt.savefig(out_path / f'panelB_semaphore_{N_ROTATIONS[ROT_IDX]}.pdf')
 plt.show()
