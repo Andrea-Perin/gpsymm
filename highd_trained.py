71aa0ca694245c8367b11f8a81989401b2057692
Andrea Perin <xinandre@gmail.com>
2024-11-13 12:12:37 +0200

    adam-like training with non-lazy params

--- highd_trained.py
+++ highd_trained.py
@@ -1,6 +1,5 @@
 # %% High-d case, but this time with trained networks instead of NTK
 import numpy as np
-from scipy.interpolate import griddata
 import jax
 from jax import numpy as jnp, random as jr
 from jaxtyping import Array, Float, Scalar, PyTree, Int, PRNGKeyArray, UInt8
@@ -15,31 +14,21 @@ from pathlib import Path
 
 from mnist_utils import load_images, load_labels, normalize_mnist
 from data_utils import three_shear_rotate, xshift_img, kronmap
-from gp_utils import kreg, circulant_error, make_circulant, extract_components
-from plot_utils import cm, cloudplot, add_spines
-
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes, InsetPosition
-from mpl_toolkits.axes_grid1 import ImageGrid
-from matplotlib.patheffects import withStroke
-plt.style.use('myplots.mlpstyle')
-plt.set_cmap('viridis')
+from gp_utils import make_circulant, circulant_error
 Ensemble = List[Tuple[Array, Array] | Tuple[()]]
 
 # %% Parameters
 SEED = 124
 RNG = jr.PRNGKey(SEED)
 N_ROTATIONS = [4, 8, 16, 32, 64]
-N_PAIRS = 1000
-
+N_PAIRS = 500
 N_IMGS = 60_000
 IN_SHAPE = 784
 HIDDEN_DIM = 512
+W_std, b_std = 1/jnp.sqrt(HIDDEN_DIM), 1/jnp.sqrt(HIDDEN_DIM)
 N_EPOCHS = 5_000
 REG = 1e-5
-N_PCS = 3
-out_path = Path('images/highd_trained')
+out_path = Path('results/highd_trained_adafactor')
 out_path.mkdir(parents=True, exist_ok=True)
 
 
@@ -51,13 +40,11 @@ make_orbit = kronmap(three_shear_rotate, 2)
 orthofft = ft.partial(jnp.fft.fft, norm='ortho')
 in_shape = images[0].flatten().shape
 # network and NTK
-W_std, b_std = 1., 1.
 init_fn, apply_fn, kernel_fn = nt.stax.serial(
     nt.stax.Dense(HIDDEN_DIM, W_std=W_std, b_std=b_std),
     nt.stax.Relu(),
     nt.stax.Dense(1, W_std=W_std, b_std=b_std)
 )
-kernel_fn = jax.jit(kernel_fn)
 
 def init_params(key):
     key, ens_key = jr.split(key)
@@ -74,16 +61,12 @@ schedule = optax.schedules.warmup_exponential_decay_schedule(
     transition_steps=100,
     decay_rate=.5
 )
-optim = optax.sgd(learning_rate=schedule)
-
-
-
+optim = optax.adafactor()
 
 
 # %%
 def loss(params: Ensemble, x: Float[Array, 'pair 2*angles 28*28'], y: Float[Array, 'pair 2*angles']) -> Scalar:
-    yhat = jax.vmap(jax.vmap(apply_fn, in_axes=(None, 0)))(params, x)
-    yhat = yhat.squeeze()
+    yhat = jax.vmap(apply_fn)(params, x).squeeze()
     mses = ein.reduce((y-yhat)**2, 'pair angle -> pair', 'mean')
     return jnp.sum(mses)
 
@@ -118,6 +101,13 @@ def train(
     return params, losses
 
 
+@jax.jit
+def get_spectral_error(data) -> Float[Array, 'N_PAIRS']:
+    kernels = jax.vmap(kernel_fn)(data, data).ntk
+    ckernels = jax.vmap(make_circulant)(kernels)
+    return jax.vmap(circulant_error)(ckernels)
+
+
 # select pairs from original mnist
 def get_data(
     key: PRNGKeyArray,
@@ -141,23 +131,32 @@ def get_data(
     return ein.rearrange(data, 'pair digit angle width height -> pair (angle digit) (width height)')
 
 
-
+sp_errors = np.empty( (len(N_ROTATIONS), N_PAIRS))
 preds = np.empty( (len(N_ROTATIONS), N_PAIRS, 2) )
+losses = np.empty( (len(N_ROTATIONS), N_EPOCHS, 2) )
 for angle_idx, n_angles in enumerate(tqdm(N_ROTATIONS)):
     data = get_data(key=RNG, n_rotations=n_angles, n_pairs=N_PAIRS)
     targets = jnp.array(([+1., -1.]*n_angles))
     targets = ein.repeat(targets, 'angle -> pair angle', pair=N_PAIRS)
+    # compute the spectral error
+    sp_errors[angle_idx] = get_spectral_error(data)
     # train on positive
     RNG, params_p = init_params(RNG)
     params_p, losses_p = train(params=params_p, x=data[:, 1:], y=targets[:, 1:], optim=optim, epochs=N_EPOCHS)
     preds_p = jax.vmap(apply_fn)(params_p, data[:, 0])
     preds[angle_idx, :, :1] = preds_p
+    losses[angle_idx, :, 0] = losses_p
+    del params_p
     # train on negative
     RNG, params_m = init_params(RNG)
     params_m, losses_m = train(params=params_m, x=data[:, :-1], y=targets[:, :-1], optim=optim, epochs=N_EPOCHS)
     preds_m = jax.vmap(apply_fn)(params_m, data[:, -1])
     preds[angle_idx, :, 1:] = preds_m
+    losses[angle_idx, :, 1] = losses_m
+    del params_m
 
 
+# save data
+np.save(out_path / 'sp_errors', sp_errors)
 np.save(out_path / 'predictions', preds)
-# %%
+np.save(out_path / 'losses', losses)
