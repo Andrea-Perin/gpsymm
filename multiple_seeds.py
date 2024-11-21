e4732d57c25f9217430c04e168f43b91bc117855
Andrea Perin <xinandre@gmail.com>
2024-11-21 19:25:08 +0200

    Added plot with nice color coding

--- multiple_seeds.py
+++ multiple_seeds.py
@@ -1,5 +1,6 @@
 # %% multiple seeds
 import numpy as np
+from numpy.ma.core import angle
 from scipy.interpolate import griddata
 import jax
 from jax import numpy as jnp, random as jr
@@ -14,7 +15,7 @@ from pathlib import Path
 from mnist_utils import load_images, load_labels, normalize_mnist
 from data_utils import three_shear_rotate, xshift_img, kronmap
 from gp_utils import kreg, circulant_error, make_circulant, extract_components
-from plot_utils import cm, cloudplot, add_spines
+from plot_utils import cm, cloudplot, add_spines, semaphore
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -28,7 +29,7 @@ plt.style.use('myplots.mlpstyle')
 SEED = 124
 TEMP = 0.
 RNG = jr.PRNGKey(SEED)
-NUM_ANGLES = 8
+ANGLES = [4, 8, 16, 32, 64]
 NUM_SEEDS = 13
 N_TESTS = 512
 REG = 1e-4
@@ -66,128 +67,147 @@ def concat_interleave(oa, ob):
     )
 
 
-def regress(points):
+def regress(points, nangles):
     whole_kernel = kernel_fn(points, points).ntk
-    idxs = jnp.arange(0, NUM_SEEDS*NUM_ANGLES, NUM_ANGLES)
+    idxs = jnp.arange(0, NUM_SEEDS*nangles, nangles)
     k11 = jnp.delete( jnp.delete(whole_kernel, idxs, axis=0), idxs, axis=1 )
     k12 = jnp.delete( whole_kernel[:, idxs], idxs, axis=0, )
     k22 = whole_kernel[idxs][:, idxs]
-    ys = jnp.array([+1.] * (NUM_SEEDS) * (NUM_ANGLES-1) + [-1.] * NUM_SEEDS * NUM_ANGLES)[:, None]
+    ys = jnp.array([+1.] * (NUM_SEEDS) * (nangles-1) + [-1.] * NUM_SEEDS * nangles)[:, None]
     sol = jax.scipy.linalg.solve(k11 + REG*jnp.eye(len(k11)), k12, assume_a='pos')
     mean = ein.einsum(sol, ys, 'train test, train d-> test d')
-    var = k22 - ein.einsum(sol, k12, 'train t1, train t2 -> t1 t2')
-    return jnp.mean(jnp.abs(1 -mean))
+    # var = k22 - ein.einsum(sol, k12, 'train t1, train t2 -> t1 t2')
+    return mean
 
 
-angles = jnp.linspace(0, 2*jnp.pi, NUM_ANGLES, endpoint=False)
-results = np.empty( (N_TESTS, 3) )
-colors = []
-RNG, test_key = jr.split(RNG)
-test_keys = jr.split(test_key, N_TESTS)
-for iteration, key in tqdm(zip(range(N_TESTS), test_keys)):
-    # pick data
-    key, kab, ka, kb = jr.split(key, num=4)
-    class_a, class_b = jnp.sort(jr.choice(kab, N_CLASSES, replace=False, shape=(2,)))
-    colors.append( (class_a, class_b) )
-    idxs_a = jr.choice(ka, jnp.argwhere(labels == class_a), replace=False, shape=(NUM_SEEDS,))[:, 0]
-    idxs_b = jr.choice(kb, jnp.argwhere(labels == class_b), replace=False, shape=(NUM_SEEDS,))[:, 0]
-    orbits_a = ein.rearrange(
-        make_orbit(images[idxs_a], angles) ,
-        'seed angle w h -> seed angle (w h)')
-    orbits_b = ein.rearrange(
-        make_orbit(images[idxs_b], angles), #+jnp.pi/NUM_ANGLES),
-        'seed angle w h -> seed angle (w h)')
+results = np.empty((len(ANGLES), N_TESTS, 3))
+colors_pairings = np.empty((len(ANGLES), N_TESTS))
+colors_semaphore = np.empty((len(ANGLES), N_TESTS))
+for angle_idx, nangles in enumerate(ANGLES):
+    angles = jnp.linspace(0, 2*jnp.pi, nangles, endpoint=False)
+    RNG, test_key = jr.split(RNG)
+    test_keys = jr.split(test_key, N_TESTS)
+    for test_idx, key in tqdm(zip(range(N_TESTS), test_keys)):
+        # pick data
+        key, kab, ka, kb = jr.split(key, num=4)
+        class_a, class_b = jnp.sort(jr.choice(kab, N_CLASSES, replace=False, shape=(2,)))
+        colors_pairings[angle_idx, test_idx] = 10*class_a+class_b  # just a way to encode pairs as colors
+        idxs_a = jr.choice(ka, jnp.argwhere(labels == class_a), replace=False, shape=(NUM_SEEDS,))[:, 0]
+        idxs_b = jr.choice(kb, jnp.argwhere(labels == class_b), replace=False, shape=(NUM_SEEDS,))[:, 0]
+        orbits_a = ein.rearrange(
+            make_orbit(images[idxs_a], angles),
+            'seed angle w h -> seed angle (w h)')
+        orbits_b = ein.rearrange(
+            make_orbit(images[idxs_b], angles),  # +jnp.pi/NUM_ANGLES),
+            'seed angle w h -> seed angle (w h)')
 
-    # First with weird
-    orbit_pairs = kronmap(concat_interleave, 2)(orbits_a, orbits_b)
-    out = jax.vmap(jax.vmap(kernel_fn), in_axes=(1,))(orbit_pairs).ntk
-    flatout = ein.rearrange(out, 'sa sb i j -> (sa sb) i j')
-    k_circ_flat = jax.vmap(make_circulant)(flatout)
+        # First with weird
+        orbit_pairs = kronmap(concat_interleave, 2)(orbits_a, orbits_b)
+        out = jax.vmap(jax.vmap(kernel_fn), in_axes=(1,))(orbit_pairs).ntk
+        flatout = ein.rearrange(out, 'sa sb i j -> (sa sb) i j')
+        k_circ_flat = jax.vmap(make_circulant)(flatout)
 
-    # considering interactions between seeds of same class
-    # orbit_pairs = kronmap(concat_interleave, 2)(orbits_a, orbits_b)
-    # k = kernel_fn(
-    #     orbit_pairs,
-    #     ein.rearrange(orbit_pairs, 'sa sb a wh -> sb sa a wh')
-    # ).ntk
-    # kflat = ein.rearrange(k, 'sa sb sap sbp i j -> (sa sb sap sbp) i j', sa=NUM_SEEDS, sb=NUM_SEEDS, sap=NUM_SEEDS, sbp=NUM_SEEDS)
-    # kflatsymm = (kflat + kflat.transpose((0, 2, 1)))/2
-    # kcirc = jax.vmap(make_circulant)(kflatsymm)
-    # sp_err = jax.vmap(circulant_error)(kcirc)
-    # sp_err = jnp.mean(sp_err)
+        # considering interactions between seeds of same class
+        # orbit_pairs = kronmap(concat_interleave, 2)(orbits_a, orbits_b)
+        # k = kernel_fn(
+        #     orbit_pairs,
+        #     ein.rearrange(orbit_pairs, 'sa sb a wh -> sb sa a wh')
+        # ).ntk
+        # kflat = ein.rearrange(k, 'sa sb sap sbp i j -> (sa sb sap sbp) i j', sa=NUM_SEEDS, sb=NUM_SEEDS, sap=NUM_SEEDS, sbp=NUM_SEEDS)
+        # kflatsymm = (kflat + kflat.transpose((0, 2, 1)))/2
+        # kcirc = jax.vmap(make_circulant)(kflatsymm)
+        # sp_err = jax.vmap(circulant_error)(kcirc)
+        # sp_err = jnp.mean(sp_err)
 
 
-    # k_mean = ein.reduce(k, '... i j -> i j', 'mean')
-    # k_circ = make_circulant(k_mean)
-    # sp_err = circulant_error(k_circ)
+        # k_mean = ein.reduce(k, '... i j -> i j', 'mean')
+        # k_circ = make_circulant(k_mean)
+        # sp_err = circulant_error(k_circ)
 
-    # maximizing the error
-    # sp_err_flat = jax.vmap(circulant_error)(k_circ_flat)
-    # sp_err = ein.rearrange(sp_err_flat, '(sa sb) -> sa sb', sa=NUM_SEEDS, sb=NUM_SEEDS)
-    # sp_err_a = ein.reduce(sp_err, 'sa sb -> sa', 'max')
-    # sp_err_a = ein.reduce(sp_err_a, 'sa ->', 'mean')
-    # sp_err_b = ein.reduce(sp_err, 'sa sb -> sb', 'max')
-    # sp_err_b = ein.reduce(sp_err_b, 'sb ->', 'mean')
-    # sp_err = (sp_err_a + sp_err_b)/2
+        # maximizing the error
+        # sp_err_flat = jax.vmap(circulant_error)(k_circ_flat)
+        # sp_err = ein.rearrange(sp_err_flat, '(sa sb) -> sa sb', sa=NUM_SEEDS, sb=NUM_SEEDS)
+        # sp_err_a = ein.reduce(sp_err, 'sa sb -> sa', 'max')
+        # sp_err_a = ein.reduce(sp_err_a, 'sa ->', 'mean')
+        # sp_err_b = ein.reduce(sp_err, 'sa sb -> sb', 'max')
+        # sp_err_b = ein.reduce(sp_err_b, 'sb ->', 'mean')
+        # sp_err = (sp_err_a + sp_err_b)/2
 
-    # errors of average (weighted)
-    # k_circ = ein.rearrange(k_circ_flat, '(sa sb) i j -> sa sb i j', sa=NUM_SEEDS, sb=NUM_SEEDS)
-    # k_circ_powers = ein.rearrange(
-    #     jax.vmap(circulant_error)(k_circ_flat),
-    #     '(sa sb) -> sa sb', sa=NUM_SEEDS, sb=NUM_SEEDS
-    # )
-    # # k_circ_powers = ein.einsum(k_circ[..., 0]**2, 'sa sb i -> sa sb')
-    # # k_circ_powers = k_circ[..., 0, 1]**2
-    # a_soft = NUM_SEEDS * jax.nn.softmax(TEMP * k_circ_powers, axis=-1)[..., None, None]
-    # b_soft = NUM_SEEDS * jax.nn.softmax(TEMP * k_circ_powers, axis=0)[..., None, None]
-    # avg_over_b = ein.reduce( k_circ  * a_soft, 'sa sb i j -> i j', 'mean' )
-    # avg_over_a = ein.reduce( k_circ  * b_soft, 'sa sb i j -> i j', 'mean' )
-    # sp_err = ( circulant_error(avg_over_a) + circulant_error(avg_over_b) ) / 2
+        # errors of average (weighted)
+        # k_circ = ein.rearrange(k_circ_flat, '(sa sb) i j -> sa sb i j', sa=NUM_SEEDS, sb=NUM_SEEDS)
+        # k_circ_powers = ein.rearrange(
+        #     jax.vmap(circulant_error)(k_circ_flat),
+        #     '(sa sb) -> sa sb', sa=NUM_SEEDS, sb=NUM_SEEDS
+        # )
+        # # k_circ_powers = ein.einsum(k_circ[..., 0]**2, 'sa sb i -> sa sb')
+        # # k_circ_powers = k_circ[..., 0, 1]**2
+        # a_soft = NUM_SEEDS * jax.nn.softmax(TEMP * k_circ_powers, axis=-1)[..., None, None]
+        # b_soft = NUM_SEEDS * jax.nn.softmax(TEMP * k_circ_powers, axis=0)[..., None, None]
+        # avg_over_b = ein.reduce( k_circ  * a_soft, 'sa sb i j -> i j', 'mean' )
+        # avg_over_a = ein.reduce( k_circ  * b_soft, 'sa sb i j -> i j', 'mean' )
+        # sp_err = ( circulant_error(avg_over_a) + circulant_error(avg_over_b) ) / 2
 
-    # average of errors
-    # sp_err_flat = jax.vmap(circulant_error)(k_circ_flat)
-    # sp_err = ein.rearrange(sp_err_flat, '(sa sb) -> sa sb', sa=NUM_SEEDS, sb=NUM_SEEDS)
-    # sp_err = ein.reduce(sp_err, 'sa sb ->', 'mean')
+        # average of errors
+        # sp_err_flat = jax.vmap(circulant_error)(k_circ_flat)
+        # sp_err = ein.rearrange(sp_err_flat, '(sa sb) -> sa sb', sa=NUM_SEEDS, sb=NUM_SEEDS)
+        # sp_err = ein.reduce(sp_err, 'sa sb ->', 'mean')
 
-    # Errors of average (unweighted)
-    k_weird = ein.reduce(out, 'seeda seedb i j -> i j', 'mean')
-    k_weird_circ = make_circulant(k_weird)
-    sp_err = circulant_error(k_weird_circ)
+        # Errors of average (unweighted)
+        k_weird = ein.reduce(out, 'seeda seedb i j -> i j', 'mean')
+        k_weird_circ = make_circulant(k_weird)
+        sp_err = circulant_error(k_weird_circ)
 
-    # Then with "normal"
-    all_points, ps = ein.pack( (orbits_a, orbits_b), '* wh' )
-    err_a = regress(all_points)
-    all_points, ps = ein.pack( (orbits_b, orbits_a), '* wh' )
-    err_b = regress(all_points)
+        # Then with "normal"
+        all_points, ps = ein.pack( (orbits_a, orbits_b), '* wh' )
+        pred_a = regress(all_points, nangles)
+        err_a = jnp.mean(1-jnp.mean(pred_a))
+        all_points, ps = ein.pack( (orbits_b, orbits_a), '* wh' )
+        pred_b = regress(all_points, nangles)
+        err_b = jnp.mean(1-jnp.mean(pred_b))
+        colors_semaphore[angle_idx, test_idx] = jnp.mean( jnp.concat((pred_a, pred_b)) > 0 )
 
-    results[iteration] = (sp_err, err_a, err_b)
+        results[angle_idx, test_idx] = (sp_err, err_a, err_b)
+
 
 # %%
-colors = jnp.array([a*10+b for a, b in colors])
-fig, ax = plt.subplots()
-ax.scatter( results[:, 0], jnp.mean(results[:, 1:], axis=1), c=colors, s=2 )
-ax.set_title(f'Error of the mean max error')
-# ax.set_title(f'Error of the weighted average (based on nn angle)')
-ax.set_xlabel('spectral')
-ax.set_ylabel('regression')
-ax.plot([0, 1.5], [0, 1.5], ls='--', color='k')
-ax.plot([0, 2], [1, 1], ls='--', color='k')
-ax.plot([1, 1], [0, 2], ls='--', color='k')
-ax.set_xlim([0, None])
-ax.set_ylim([0, None])
-# plt.savefig(f'multiple_seeds_mean_of_max_err.pdf')
-plt.show()
-# # %%
-# fig, ax = plt.subplots()
-# ax.scatter( results[:, 0], results[:, 1], c=colors )
-# ax.set_xlabel('spectral')
-# ax.set_ylabel('regression')
-# ax.plot([0, 1.5], [0, 1.5], ls='--')
-# plt.show()
+figsize = (16*cm, 6*cm)
+fig = plt.figure(figsize=figsize)
+grid = ImageGrid(fig, 111,
+                nrows_ncols=(1, len(ANGLES)),
+                axes_pad=0.15,
+                share_all=True,
+                cbar_location="right",
+                cbar_mode="single",
+                cbar_size="5%",
+                cbar_pad=0.15,
+                )
 
-# fig, ax = plt.subplots()
-# ax.scatter( results[:, 0], results[:, 2], c=colors )
-# ax.set_xlabel('spectral')
-# ax.set_ylabel('regression')
-# ax.plot([0, 1.5], [0, 1.5], ls='--')
-# plt.show()
+for angle_idx, ax in enumerate(grid):
+    sc = ax.scatter(
+        np.mean(results[angle_idx, :, 1:], axis=1),
+        results[angle_idx, :, 0],
+        c=colors_semaphore[angle_idx],
+        cmap='RdYlGn',
+        vmin=0,
+        vmax=1,
+        s=2
+    )
+    ax.set_title(f'$N_{{rot}}={{{ANGLES[angle_idx]}}}$', fontsize=10)
+    ax.plot([0, 1.5], [0, 1.5], ls='--', color='k', lw=.5)
+    ax.plot([0, 2], [1, 1], ls='--', color='k', lw=.5)
+    ax.plot([1, 1], [0, 2], ls='--', color='k', lw=.5)
+    ax.set_xlim([0, 2])
+    ax.set_ylim([0, 2])
+    ax.set_xticks([0, 1, 2])
+    ax.set_yticks([0, 1, 2])
+
+# Add labels
+fig.text(0.5, 0.15, 'Spectral error', ha='center', fontsize=10)
+fig.text(0.0, 0.5, 'Empirical error', va='center', rotation='vertical', fontsize=10)
+
+# Colorbar will be automatically added to the last image
+grid.cbar_axes[0].colorbar(sc, format=lambda x, _: f'{100*x:.0f}%')
+grid.cbar_axes[0].set_ylabel('Corr. class \%', fontsize=10)
+
+plt.tight_layout()
+plt.show()
