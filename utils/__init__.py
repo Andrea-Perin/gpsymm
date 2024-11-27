0ba0ac76f04adb82048f623e29fb0f4b5ed17377
Andrea Perin <xinandre@gmail.com>
2024-11-27 18:05:32 +0200

    organizing code

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 from .gp_utils import make_circulant, extract_components, kreg, circulant_predict, circulant_error
-from .data_utils import kronmap, three_shear_rotate
+from .data_utils import kronmap, three_shear_rotate, make_rotation_orbit, scipy_rotate
 from .mnist_utils import load_images, load_labels, normalize_mnist
+from .plot_utils import cm, semaphore, add_spines
 
 __all__ = [
     'make_circulant',
@@ -8,8 +9,12 @@ __all__ = [
     'kreg',
     'circulant_predict',
     'circulant_error',
+
     'kronmap',
     'three_shear_rotate',
+    'scipy_rotate',
+    'make_rotation_orbit',
+
     'load_images',
     'load_labels',
     'normalize_mnist'
